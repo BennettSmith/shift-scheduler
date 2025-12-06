f32f42ca36a4a0d0bb20b5dab18bcb16af7cd62a
Bennett Smith <bennettsmith@mac.com>
2025-12-06 15:39:29 -0800

    chore:  Adds Xcode project

---
+++ ios/ShiftScheduler/ShiftSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftSchedulerApp.swift
+//  ShiftScheduler
+//
+//  Created by Bennett Smith on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ShiftSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/ShiftSchedulerTests/ShiftSchedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftSchedulerTests.swift
+//  ShiftSchedulerTests
+//
+//  Created by Bennett Smith on 12/6/25.
+//
+
+import Testing
+@testable import ShiftScheduler
+
+struct ShiftSchedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
